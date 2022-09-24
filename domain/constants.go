3fd15e45fc6dc54a9be96815d728c7170b3d829b
Andreia Silva <andreia.silva@toggl.com>
2022-09-24 12:49:50 -0300

    Improving commands nomenclature

--- domain/constants.go
+++ domain/constants.go
@@ -5,14 +5,14 @@ import eh "github.com/looplab/eventhorizon"
 // WalletAggregateType represent the wallet aggregate type.
 const WalletAggregateType eh.AggregateType = "wallet"
 
-// Possible Commands.
+// Commands.
 const (
-	WalletCreationCommand eh.CommandType = "create"
-	WalletCreditCommand   eh.CommandType = "credit"
-	WalletDebitCommand    eh.CommandType = "debit"
+	WalletCreationCommand eh.CommandType = "create_wallet"
+	WalletCreditCommand   eh.CommandType = "credit_amount"
+	WalletDebitCommand    eh.CommandType = "debit_amount"
 )
 
-// Possible Events.
+// Events.
 const (
 	WalletCreatedEvent  eh.EventType = "wallet_created"
 	WalletCreditedEvent eh.EventType = "amount_credited"
